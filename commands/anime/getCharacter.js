b1b2d267947e690aaec1545f4d26736181188189
arescrimson <areszee7@gmail.com>
2023-12-17 23:09:38 -0800

    Update getCharacter.js
    Updated into class-based module, updated and refined helper methods, refactored to use class instances, added getters.

--- commands/anime/getCharacter.js
+++ commands/anime/getCharacter.js
@@ -1,6 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const { getJikanID } = require('../../utils/jikan/getJikanID');
-const { getAnimeCharacters } = require('../../utils/anime/getCharacter');
+const { getAnimeCharacters, getNextCharacter, AnimeCharacterSearch } = require('../../utils/anime/getCharacter');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,29 +17,54 @@ module.exports = {
                 .setName('animename')
                 .setDescription('Name of anime')
                 .setRequired(true)
-        )
-        ,
+        ),
     async execute(interaction) {
         try {
-            await interaction.deferReply(); 
-            const animeName = await interaction.options.getString('animename');
-            const animeID = await getJikanID(animeName); 
-            const characterName = await interaction.options.getString('charactername');
-            const embed = await getAnimeCharacters(characterName, animeID); 
+            await interaction.deferReply();
 
-            if (embed) {
-                interaction.editReply({ embeds: [embed] });
-            } else { 
-                interaction.editReply('couldnt find character.'); 
-            }
+            const animeName = await interaction.options.getString('animename');
+            const characterName = await interaction.options.getString('charactername');
+            const animeID = await getJikanID('anime', animeName);
+
+            if (!animeID) return
+
+            const animeCharacterSearch = new AnimeCharacterSearch(characterName, animeID); 
+            const embed = await animeCharacterSearch.getAnimeCharacters()
+
+            const right = new ButtonBuilder()
+                .setCustomId('right')
+                .setLabel('-->')
+                .setStyle(ButtonStyle.Success);
+
+            const left = new ButtonBuilder()
+                .setCustomId('left')
+                .setLabel('<--')
+                .setStyle(ButtonStyle.Success);
+
+            const row = new ActionRowBuilder()
+                .addComponents(left, right);
+
+            const response = await interaction.editReply({
+                embeds: [embed],
+                components: [row],
+            });
+
+            const collectorFilter = i => i.user.id === interaction.user.id;
+
+            const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 20000 });
+
+            collector.on('collect', async (buttonInteraction) => {
+                if (buttonInteraction.customId === 'right') {
+                    console.log(animeCharacterSearch.getCharacterArr()); 
+                    console.log(animeCharacterSearch.getCharacterName());
+                } else if (buttonInteraction.customId === 'left') {
+                    console.log('left');
+                }
+            });
+            
         } catch (error) {
-            console.log('error in getCharacters' + error);
+            console.error('Error in getCharacters', error);
+            interaction.followUp('An error occurred.');
         }
     },
 };
-
-
-
-
-
-
