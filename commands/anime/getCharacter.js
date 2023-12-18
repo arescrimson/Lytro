7ba1cb5178ada8fe772b0fa1db0ea90ff66cdb94
arescrimson <areszee7@gmail.com>
2023-12-18 00:47:34 -0800

    Update getCharacter.js
    Updated defer and button press functionality.

--- commands/anime/getCharacter.js
+++ commands/anime/getCharacter.js
@@ -20,51 +20,66 @@ module.exports = {
         ),
     async execute(interaction) {
         try {
-            await interaction.deferReply();
-
+            await interaction.deferReply(); 
             const animeName = await interaction.options.getString('animename');
             const characterName = await interaction.options.getString('charactername');
             const animeID = await getJikanID('anime', animeName);
 
-            if (!animeID) return
+            if (!animeID) return;
 
-            const animeCharacterSearch = new AnimeCharacterSearch(characterName, animeID); 
+            const animeCharacterSearch = new AnimeCharacterSearch(characterName, animeID);
             const embed = await animeCharacterSearch.getAnimeCharacters()
+            console.log(animeCharacterSearch.getCharacterName()); 
 
-            const right = new ButtonBuilder()
-                .setCustomId('right')
-                .setLabel('-->')
-                .setStyle(ButtonStyle.Success);
+            if (animeCharacterSearch.getCharacterArr().length > 1) {
+                
+                const right = new ButtonBuilder()
+                    .setCustomId('right')
+                    .setLabel('-->')
+                    .setStyle(ButtonStyle.Success);
 
-            const left = new ButtonBuilder()
-                .setCustomId('left')
-                .setLabel('<--')
-                .setStyle(ButtonStyle.Success);
+                const left = new ButtonBuilder()
+                    .setCustomId('left')
+                    .setLabel('<--')
+                    .setStyle(ButtonStyle.Success);
 
-            const row = new ActionRowBuilder()
-                .addComponents(left, right);
+                const row = new ActionRowBuilder()
+                    .addComponents(left, right);
 
-            const response = await interaction.editReply({
-                embeds: [embed],
-                components: [row],
-            });
+                const response = await interaction.editReply({
+                    embeds: [embed],
+                    components: [row],
+                });
 
-            const collectorFilter = i => i.user.id === interaction.user.id;
+                const collectorFilter = i => i.user.id === interaction.user.id;
 
-            const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 20000 });
+                const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 20000 });
 
-            collector.on('collect', async (buttonInteraction) => {
-                if (buttonInteraction.customId === 'right') {
-                    console.log(animeCharacterSearch.getCharacterArr()); 
-                    console.log(animeCharacterSearch.getCharacterName());
-                } else if (buttonInteraction.customId === 'left') {
-                    console.log('left');
-                }
-            });
-            
+                let updatedEmbed; 
+
+                collector.on('collect', async buttonInteraction => {
+                    try {
+                        await buttonInteraction.deferUpdate(); 
+                        
+                        if (buttonInteraction.customId === 'right') {
+                            updatedEmbed = await animeCharacterSearch.updateCharacterEmbed(true);
+                            await interaction.editReply({ embeds: [updatedEmbed] })
+                        } else if (buttonInteraction.customId === 'left') {
+                            updatedEmbed = await animeCharacterSearch.updateCharacterEmbed(false);
+                            await interaction.editReply({ embeds: [updatedEmbed] })
+                        }
+                    } catch (error) {
+                        console.error(error); 
+                    }
+                });
+            } else {
+                await interaction.editReply({
+                    embeds: [embed],
+                });
+            }
+
         } catch (error) {
             console.error('Error in getCharacters', error);
-            interaction.followUp('An error occurred.');
         }
     },
 };
