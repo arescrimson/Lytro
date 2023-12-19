05ae15996b672f39e84c3e615c072467344dd731
arescrimson <areszee7@gmail.com>
2023-12-19 13:40:35 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,6 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
-
-
 module.exports = {
     DISCORD_TOKEN,
     CLIENT_ID,
