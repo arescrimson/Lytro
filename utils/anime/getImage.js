2dbfcc40de6604317dbeb2823a8536a660b38194
arescrimson <areszee7@gmail.com>
2023-12-18 23:44:24 -0800

    Create getImage.js

---
+++ utils/anime/getImage.js
@@ -0,0 +1,61 @@
+const { JIKAN_CLIENT } = require('../jikan/jikanClient');
+const { createAnimeImageEmbed } = require('../embed/createAnimeEmbeds');
+
+class AnimeImageSearch {
+
+    constructor(animeID) {
+        this.animeID = animeID;
+        this.animeImageEmbed = null;
+        this.animeImageArray = [];
+        this.searchedSet = new Set();
+    }
+
+    async createAnimeImagesEmbed() {
+
+        try {
+
+            let randomImageIndex;
+
+            const anime = await JIKAN_CLIENT.anime.get(this.animeID);
+            this.animeImageArray = await JIKAN_CLIENT.anime.getPictures(this.animeID);
+            if (!this.animeImageArray) return null; 
+
+            //randomly search index of picture gallery 
+            do {
+                randomImageIndex = Math.floor(Math.random() * this.animeImageArray.length);
+            }
+            //If set contains an index it's already searched, runs random search again. 
+            while (this.searchedSet.has(randomImageIndex));
+
+            //If searched images Set exceeds the length of picture gallery array, clear values from Set. 
+            if (this.searchedSet.size >= this.animeImageArray.length) {
+                this.searchedSet.clear();
+            }
+            //Else, add index to searched index Set. 
+            else {
+                this.searchedSet.add(randomImageIndex);
+            }
+
+            const pictureLink = this.animeImageArray[randomImageIndex].webp.default.href;
+
+            this.animeImageEmbed = createAnimeImageEmbed(anime.title.default, pictureLink);
+
+            return this.animeImageEmbed;
+
+        } catch (error) {
+            console.error('Error in getAnimeImages:', error.message);
+        }
+    }
+
+    getAnimeImageEmbed() {
+        return this.animeImageEmbed;
+    }
+
+    getAnimeImageArrayLength() {
+        return this.animeImageArray.length;
+    }
+}
+
+module.exports = {
+    AnimeImageSearch
+}
