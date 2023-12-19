9347ece3c938360db6a98d1bad623d8d6ea042eb
arescrimson <areszee7@gmail.com>
2023-12-19 00:03:20 -0800

    Create config.js
    Created config

---
+++ config.js
@@ -0,0 +1,9 @@
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID; 
+
+module.exports = { 
+    DISCORD_TOKEN, 
+    CLIENT_ID,
+	GUILD_ID
+}
