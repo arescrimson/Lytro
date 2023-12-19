5a189b1aafd3c1b76126480c81a4056aa9c5ca2b
arescrimson <areszee7@gmail.com>
2023-12-18 23:44:26 -0800

    Create getRandom.js

---
+++ utils/anime/getRandom.js
@@ -0,0 +1,14 @@
+const { JIKAN_CLIENT } = require('../jikan/jikanClient');
+
+async function getRandomID() {
+    try {
+        const random = await JIKAN_CLIENT.anime.random();
+        return random.id;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    getRandomID
+}
