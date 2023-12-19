b3fb27b6c2a3778fa281470291c34e96ac01c9dd
arescrimson <areszee7@gmail.com>
2023-12-18 23:43:08 -0800

    Update getAnime.js
    Updated getAnime button interactions and functionalities

--- commands/anime/getAnime.js
+++ commands/anime/getAnime.js
@@ -1,31 +1,72 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const { getJikanID } = require('../../utils/jikan/getJikanID');
-const { getAnimeInfo } = require('../../utils/anime/getAnime');
+const { AnimeSearch } = require('../../utils/anime/getAnime');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('anime')
+		.setName('a')
 		.setDescription('gets anime.')
 		.addStringOption(option =>
 			option
-				.setName('name')
-				.setDescription('Name of anime')
+				.setName('animename')
+				.setDescription('Name of Anime')
 				.setRequired(true)
 		),
 	async execute(interaction) {
 		try {
-			await interaction.deferReply(); 
-			const animeName = await interaction.options.getString('name');
-			const animeID = await getJikanID('anime', animeName);
-			const embed = await getAnimeInfo(animeID);
+			await interaction.deferReply().then(() => console.log('...'));
 
-			if (embed) { 
-				await interaction.editReply({ embeds: [embed]});
-			} else { 
-				await interaction.editReply('couldnt find animeID.');
+			const animeName = await interaction.options.getString('animename');
+			const animeID = await getJikanID('anime', animeName);
+
+			if (!animeID) { 
+				await interaction.editReply('Anime not Found.'); 
+				return; 
 			}
-		} catch(theErr) { 
-			console.error(theErr); 
+			const animeSearch = new AnimeSearch(animeID);
+			const animeEmbed = await animeSearch.createAnimeEmbed();
+			const animeInfoEmbed = await animeSearch.createAnimeInfoEmbed();
+
+			const right = new ButtonBuilder()
+				.setCustomId('right')
+				.setLabel('-->')
+				.setStyle(ButtonStyle.Success);
+
+			const left = new ButtonBuilder()
+				.setCustomId('left')
+				.setLabel('<--')
+				.setStyle(ButtonStyle.Success);
+
+			const row = new ActionRowBuilder()
+				.addComponents(left, right);
+
+			const response = await interaction.editReply({
+				embeds: [animeEmbed],
+				components: [row],
+			});
+
+			const collectorFilter = i => i.user.id === interaction.user.id;
+
+			const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 60000 });
+
+			collector.on('collect', async buttonInteraction => {
+				try {
+					await buttonInteraction.deferUpdate();
+
+					if (buttonInteraction.customId === 'right') {
+						const updatedEmbed = animeSearch.getAnimeInfoEmbed();
+						await interaction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+					} else if (buttonInteraction.customId === 'left') {
+						const updatedEmbed = animeSearch.getAnimeEmbed();
+						await interaction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			});
+
+		} catch (theErr) {
+			console.error(theErr);
 		}
 	},
 };
