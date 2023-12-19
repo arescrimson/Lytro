58b0c69b71d30064e7f3211fad844f22fd8ad0e5
arescrimson <areszee7@gmail.com>
2023-12-19 00:18:01 -0800

    Update config.js
    Updated with embed icons and error messages.

--- config.js
+++ config.js
@@ -1,9 +1,28 @@
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID; 
+const GUILD_ID = process.env.GUILD_ID;
 
-module.exports = { 
-    DISCORD_TOKEN, 
+
+
+module.exports = {
+    DISCORD_TOKEN,
     CLIENT_ID,
-	GUILD_ID
+    GUILD_ID,
+    THUMBNAIL: 'https://github.com/arescrimson/Lytro/blob/master/media/profile.jpg?raw=true',
+    ICON_URL: 'https://github.com/arescrimson/Lytro/blob/master/media/icon.png?raw=true',
+    MAX_VALUE_LENGTH: 1020,
+    SYNOPSIS_NOT_FOUND: 'Synopsis not listed.',
+    URL_NOT_FOUND: 'URL not listed.',
+    EPISODES_NOT_FOUND: 'Episodes not listed.',
+    GENRES_NOT_FOUND: 'Genres not listed.',
+    RATINGS_NOT_FOUND: 'Ratings not listed.',
+    BACKGROUND_NOT_FOUND: 'Background not listed.',
+    YEAR_NOT_FOUND: 'Year not listed.',
+    TRAILER_NOT_FOUND: 'Trailer not listed.',
+    STUDIO_NOT_FOUND: 'Studios not listed.',
+    RECOMMENDATIONS_NOT_FOUND: 'Recommendations not listed.',
+    DESCRIPTION_NOT_FOUND: 'Description not listed.',
+    ROLE_NOT_FOUND: 'Role not listed.',
+    VA_NOT_FOUND: 'Voice Actor not listed.',
+
 }
