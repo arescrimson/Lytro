3e64c8ec85a979bebb775dbcd55f4e290c82739d
arescrimson <areszee7@gmail.com>
2023-12-19 00:47:36 -0800

    Create getManga.js

---
+++ commands/manga/getManga.js
@@ -0,0 +1,78 @@
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+const { getJikanID } = require('../../utils/jikan/getJikanID');
+const { MangaSearch } = require('../../utils/manga/getManga');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('m')
+		.setDescription('gets manga.')
+		.addStringOption(option =>
+			option
+				.setName('manganame')
+				.setDescription('Name of Manga')
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		try {
+			await interaction.deferReply();
+
+			const mangaName = await interaction.options.getString('manganame');
+			const mangaID = await getJikanID('manga', mangaName);
+
+			if (!mangaID) { 
+				await interaction.editReply('Manga not Found.'); 
+				return; 
+			}
+			const mangaSearch = new MangaSearch(mangaID);
+			const mangaEmbed = await mangaSearch.createMangaEmbed();
+			const mangaInfoEmbed = await mangaSearch.createMangaInfoEmbed();
+
+			const right = new ButtonBuilder()
+				.setCustomId('right')
+				.setLabel('-->')
+				.setStyle(ButtonStyle.Success);
+
+			const left = new ButtonBuilder()
+				.setCustomId('left')
+				.setLabel('<--')
+				.setStyle(ButtonStyle.Success);
+
+			const row = new ActionRowBuilder()
+				.addComponents(left, right);
+
+			const response = await interaction.editReply({
+				embeds: [mangaEmbed],
+				components: [row],
+			});
+
+			const collectorFilter = i => i.user.id === interaction.user.id;
+
+			const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 60000 });
+
+			collector.on('collect', async buttonInteraction => {
+				try {
+					await buttonInteraction.deferUpdate();
+
+					if (buttonInteraction.customId === 'right') {
+						const updatedEmbed = mangaSearch.getMangaInfoEmbed();
+						await interaction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+					} else if (buttonInteraction.customId === 'left') {
+						const updatedEmbed = mangaSearch.getMangaEmbed();
+						await interaction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			});
+
+		} catch (theErr) {
+			console.error(theErr);
+		}
+	},
+};
+
+
+
+
+
+
