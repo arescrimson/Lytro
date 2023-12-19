298436aa4d7ead3ef32d8e8e998d6a222c2dd8ae
arescrimson <areszee7@gmail.com>
2023-12-19 00:03:30 -0800

    Update dc.js
    Update config json to js

--- dc.js
+++ dc.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, DISCORD_TOKEN } = require('./config.json');
+const { clientId, guildId, DISCORD_TOKEN } = require('./config');
 const fs = require('node:fs');
 const path = require('node:path');
 
