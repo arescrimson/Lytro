e6d4ef1b12c39d94fd89eeb2d7a740b5d33cda59
arescrimson <areszee7@gmail.com>
2023-12-21 01:01:02 -0800

    Added random manga util
    Added random manga command.

---
+++ commands/manga/getMangaRandom.js
@@ -0,0 +1,69 @@
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+const { MangaSearch } = require('../../utils/manga/getManga');
+const { getRandomMangaID } = require('../../utils/manga/getMangaRandom');
+const { rightArrowText, leftArrowText } = require('../../config');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('mrand')
+		.setDescription('gets random manga.')
+        ,
+	async execute(interaction) {
+		try {
+			await interaction.deferReply();
+
+            const mangaID = await getRandomMangaID(); 
+
+			const mangaSearch = new MangaSearch(mangaID);
+			const mangaEmbed = await mangaSearch.createMangaEmbed();
+			const mangaInfoEmbed = await mangaSearch.createMangaInfoEmbed();
+
+			const right = new ButtonBuilder()
+				.setCustomId('right')
+				.setLabel(rightArrowText)
+				.setStyle(ButtonStyle.Primary);
+
+			const left = new ButtonBuilder()
+				.setCustomId('left')
+				.setLabel(leftArrowText)
+				.setStyle(ButtonStyle.Primary);
+
+			const row = new ActionRowBuilder()
+				.addComponents(left, right);
+
+			const response = await interaction.editReply({
+				embeds: [mangaEmbed],
+				components: [row],
+			});
+
+			const collectorFilter = i => i.user.id === interaction.user.id;
+
+			const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 60000 });
+
+			collector.on('collect', async buttonInteraction => {
+				try {
+					await buttonInteraction.deferUpdate();
+
+					if (buttonInteraction.customId === 'right') {
+						const updatedEmbed = mangaSearch.getMangaInfoEmbed();
+						await interaction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+					} else if (buttonInteraction.customId === 'left') {
+						const updatedEmbed = mangaSearch.getMangaEmbed();
+						await interaction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			});
+
+		} catch (theErr) {
+			console.error(theErr);
+		}
+	},
+};
+
+
+
+
+
+
