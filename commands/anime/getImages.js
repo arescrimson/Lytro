3df7796552285a787a8b38ca5b7bce08dcfcaa9c
arescrimson <areszee7@gmail.com>
2023-12-21 16:45:15 -0800

    Update getImages.js
    Added autofill menu to getImages.

--- commands/anime/getImages.js
+++ commands/anime/getImages.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const { getJikanID } = require('../../utils/jikan/getJikanID');
 const { AnimeImageSearch } = require('../../utils/anime/getImage');
+const { getAnimeArray } = require('../../utils/anime/getAnime');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,30 +12,45 @@ module.exports = {
                 .setName('anime')
                 .setDescription('Name of Anime')
                 .setRequired(true)
+                .setAutocomplete(true)
         ),
+    async autocomplete(interaction) {
+        const focusedValue = await interaction.options.getFocused();
+
+        const animeArray = await getAnimeArray(focusedValue);
+
+        const animeNames = animeArray.filter(name =>
+            (name.title && name.title.english !== null) &&
+            name.title.english.toLowerCase().startsWith(focusedValue.toLowerCase())
+        );
+
+        const limitedAnimeList = animeNames.slice(0, 15)
+
+        await interaction.respond(
+            limitedAnimeList.map(names => ({ name: names.title.english, value: names.title.english }))
+        );
+    },
     async execute(interaction) {
         try {
-            await interaction.deferReply(); 
+            await interaction.deferReply();
             const animeName = await interaction.options.getString('anime');
             const animeID = await getJikanID('anime', animeName);
 
-            if (!animeID) { 
-                await interaction.editReply('Anime not Found.'); 
+            if (!animeID) {
+                await interaction.editReply('Anime not Found.');
                 return;
             }
 
             const animeImageSearch = new AnimeImageSearch(animeID);
-            const animeImageEmbed = await animeImageSearch.createAnimeImagesEmbed(); 
+            const animeImageEmbed = await animeImageSearch.createAnimeImagesEmbed();
 
-            if (!animeImageEmbed) { 
-                await interaction.editReply('Images not Found'); 
+            if (!animeImageEmbed || animeImageSearch.getAnimeImageArrayLength() === 0) { 
+                interaction.editReply({content: 'no images could be found', ephemeral: true});
                 return; 
-            }
+            } 
 
-            if (animeImageSearch.getAnimeImageArrayLength() === 0) return; 
-            
             if (animeImageSearch.getAnimeImageArrayLength() > 1) {
-                
+
                 const newImage = new ButtonBuilder()
                     .setCustomId('newImage')
                     .setLabel('+')
@@ -48,18 +64,16 @@ module.exports = {
                     components: [row],
                 });
 
-                const collectorFilter = i => i.user.id === interaction.user.id;
-
                 const collector = response.createMessageComponentCollector({ time: 60000 });
 
                 collector.on('collect', async buttonInteraction => {
                     try {
                         if (buttonInteraction.customId === 'newImage') {
-                            const updatedEmbed = await animeImageSearch.createAnimeImagesEmbed(); 
-                            await buttonInteraction.update({embeds: [updatedEmbed]})
-                        } 
+                            const updatedEmbed = await animeImageSearch.createAnimeImagesEmbed();
+                            await buttonInteraction.update({ embeds: [updatedEmbed] })
+                        }
                     } catch (error) {
-                        console.error(error); 
+                        console.error(error);
                     }
                 });
             } else {
