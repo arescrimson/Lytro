23514581a27e04d044c4387edb7f8fe4c5989a1b
arescrimson <areszee7@gmail.com>
2023-12-22 19:23:00 -0800

    Update getSteamGame.js

--- commands/steam/getSteamGame.js
+++ commands/steam/getSteamGame.js
@@ -46,7 +46,7 @@ module.exports = {
             const gameEmbed = await steamGameSearch.createGameEmbed();
 
             if (!gameEmbed) {
-                interaction.editReply('Game not listed/found.');
+                interaction.editReply('Game not found/listed.');
                 return;
             }
 
