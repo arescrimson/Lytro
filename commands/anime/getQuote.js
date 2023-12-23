b17d7931876bb1628776a3c698982b63a882518c
arescrimson <areszee7@gmail.com>
2023-12-22 19:22:52 -0800

    Create getQuote.js

---
+++ commands/anime/getQuote.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getQuote } = require('../../utils/misc/getQuote'); 
+
+module.exports = {
+    data:
+        new SlashCommandBuilder()
+            .setName('quote')
+            .setDescription('return random quote')
+    ,
+    async execute(interaction) {
+        try {
+            await interaction.deferReply()
+            const quoteEmbed = await getQuote(); 
+
+            if (!quoteEmbed) { 
+                await interaction.editReply('Something went wrong getting that quote.')
+            }
+
+            await interaction.editReply({
+                embeds: [quoteEmbed]
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
