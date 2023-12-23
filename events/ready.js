05c2e03f3df2c22c48c20f1527977876e7eaff15
arescrimson <areszee7@gmail.com>
2023-12-22 21:29:10 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Lytro has been added to ${client.guilds.cache.size} discord servers.`);
 	},
 };
