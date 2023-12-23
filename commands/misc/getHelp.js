b2ab92ddd01b520efb8ad6c7241ffd842d77ddc0
arescrimson <areszee7@gmail.com>
2023-12-22 19:22:56 -0800

    Create getHelp.js

---
+++ commands/misc/getHelp.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { createHelpEmbed } = require('../../utils/embed/createEmbeds'); 
+
+module.exports = {
+    data:
+        new SlashCommandBuilder()
+            .setName('help')
+            .setDescription('information about commands.')
+    ,
+    async execute(interaction) {
+        try {
+            await interaction.deferReply()
+            
+            const helpEmbed = createHelpEmbed(); 
+
+            await interaction.editReply({
+                embeds: [helpEmbed]
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
