856836dd26c82cf44d078563dbd62f24f91820d8
arescrimson <areszee7@gmail.com>
2023-12-25 15:04:52 -0800

    Create getCharacter.test.js
    Updated getCharacter tests

---
+++ tests/getCharacter.test.js
@@ -0,0 +1,32 @@
+const { AnimeCharacterSearch } = require('../utils/anime/getCharacter');
+const { JIKAN_CLIENT } = require('../utils/jikan/jikanClient');
+
+class AnimeCharacterSearchTests {
+    constructor() {
+        this.animeCharacterSearch = new AnimeCharacterSearch('main');
+        this.characterArr = []; 
+    }
+
+    testInitialization() {
+        test('AnimeSearch initialization', () => {
+            expect(this.animeCharacterSearch.getCharacterName()).toBe('main');
+        });
+    }
+
+    testCharacterArr() { 
+        test('CharacterArr', async () => { 
+            const jikanCharacterArr = await JIKAN_CLIENT.anime.getCharacters(1)
+
+            this.characterArr = this.animeCharacterSearch.createCharacterArray(jikanCharacterArr);
+            
+            expect(this.characterArr.length).toBe(4);
+        })
+    }
+
+}
+
+const animeCharacterSearchTests = new AnimeCharacterSearchTests(); 
+animeCharacterSearchTests.testInitialization();
+animeCharacterSearchTests.testCharacterArr();
+
+
