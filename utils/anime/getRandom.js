8d0b71f4567f612d7b6db62ba94d80139aeabdb4
arescrimson <areszee7@gmail.com>
2023-12-26 18:28:33 -0800

    Updated random check

--- utils/anime/getRandom.js
+++ utils/anime/getRandom.js
@@ -2,7 +2,7 @@ const { JIKAN_CLIENT } = require('../jikan/jikanClient');
 
 async function getRandomID() {
     try {
-        const random = await JIKAN_CLIENT.anime.random(); 
+        const random = await JIKAN_CLIENT.anime.random(false); 
 
         return random.id;
 
--- utils/manga/getRandom.js
+++ utils/manga/getRandom.js
@@ -3,7 +3,7 @@ const { JIKAN_CLIENT } = require('../jikan/jikanClient');
 async function getRandomMangaID() {
     try {
         
-        const random = await JIKAN_CLIENT.manga.random();
+        const random = await JIKAN_CLIENT.manga.random(false);
 
         return random.id;
 
