f895f990b189364de33485e94482124965e7f220
arescrimson <areszee7@gmail.com>
2023-12-30 19:44:51 -0800

    Update getJikanID.js
    Formatting

--- utils/jikan/getJikanID.js
+++ utils/jikan/getJikanID.js
@@ -1,29 +1,29 @@
-const {JIKAN_CLIENT} = require('../jikan/jikanClient');
+const { JIKAN_CLIENT } = require('../jikan/jikanClient');
 
 async function getJikanID(mediaType, searchString) {
     try {
-        let searchResults; 
-        
-        switch(mediaType) { 
-            case 'anime': 
+        let searchResults;
+
+        switch (mediaType) {
+            case 'anime':
                 searchResults = await JIKAN_CLIENT.anime.search(searchString);
-                break; 
-            case 'manga': 
+                break;
+            case 'manga':
                 searchResults = await JIKAN_CLIENT.manga.search(searchString);
-                break; 
+                break;
         }
 
-        if (!searchResults) return null; 
+        if (!searchResults) return null;
 
         const filteredResult = searchResults.find(anime =>
-			(anime?.title && anime?.title?.english !== null) && 
-			anime.title.english.toLowerCase() === searchString.toLowerCase()
-		);
+            (anime?.title && anime?.title?.english !== null) &&
+            anime.title.english.toLowerCase() === searchString.toLowerCase()
+        );
 
-        if (!filteredResult) return null; 
+        if (!filteredResult) return null;
 
-        return filteredResult.id; 
-        
+        return filteredResult.id;
+
     } catch (error) {
         console.error('Error in getting JikanID', error.message);
     }
