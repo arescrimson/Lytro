2b22bddbf52186f3da8504aca9bf5ec7fb99e91a
arescrimson <areszee7@gmail.com>
2023-12-30 18:52:58 -0800

    Update getAnime.js
    began process of merging anime pages 1 and 2.

--- commands/anime/getAnime.js
+++ commands/anime/getAnime.js
@@ -41,7 +41,7 @@ module.exports = {
 			const animeID = await getJikanID('anime', animeName);
 
 			if (!animeID) {
-				await interaction.editReply('Anime not Listed/Found. log ssh');
+				await interaction.editReply('Anime not Listed/Found.');
 				return;
 			}
 
@@ -49,23 +49,13 @@ module.exports = {
 			const animeEmbed = await animeSearch.createAnimeEmbed();
 			const animeInfoEmbed = await animeSearch.createAnimeInfoEmbed();
 
-			const left = new ButtonBuilder()
-				.setCustomId('left')
-				.setLabel('Page 1')
-				.setStyle(ButtonStyle.Primary);
-
-			const right = new ButtonBuilder()
-				.setCustomId('right')
-				.setLabel('Page 2')
-				.setStyle(ButtonStyle.Primary);
-
 			const chr = new ButtonBuilder()
 				.setCustomId('chr')
 				.setLabel('Main Characters')
 				.setStyle(ButtonStyle.Success);
 
 			const row = new ActionRowBuilder()
-				.addComponents(left, right, chr);
+				.addComponents(chr);
 
 			const response = await interaction.editReply({
 				embeds: [animeEmbed],
@@ -78,64 +68,51 @@ module.exports = {
 				try {
 					await buttonInteraction.deferUpdate();
 
-					switch (buttonInteraction.customId) {
-						case 'left':
-							const animeleftEmbed = animeSearch.getAnimeEmbed();
-							await buttonInteraction.editReply({ embeds: [animeleftEmbed] }).catch(console.error)
-							break;
-						case 'right':
-							const animeRightEmbed = animeSearch.getAnimeInfoEmbed();
-							await buttonInteraction.editReply({ embeds: [animeRightEmbed] }).catch(console.error);
-							break;
-						case 'chr':
-							const animeObj = animeSearch.getAnimeObj();
-							const animeCharacterSearch = new AnimeCharacterSearch('main');
-							animeCharacterSearch.setSearchMain(true);
-							const animeCharacterEmbed = await animeCharacterSearch.createMainCharacterEmbed(animeObj);
+					const animeObj = animeSearch.getAnimeObj();
+					const animeCharacterSearch = new AnimeCharacterSearch('main');
+					animeCharacterSearch.setSearchMain(true);
+					const animeCharacterEmbed = await animeCharacterSearch.createMainCharacterEmbed(animeObj);
 
-							const right = new ButtonBuilder()
-								.setCustomId('right')
-								.setLabel(rightArrowText)
-								.setStyle(ButtonStyle.Primary);
+					const right = new ButtonBuilder()
+						.setCustomId('right')
+						.setLabel(rightArrowText)
+						.setStyle(ButtonStyle.Primary);
 
-							const left = new ButtonBuilder()
-								.setCustomId('left')
-								.setLabel(leftArrowText)
-								.setStyle(ButtonStyle.Primary);
+					const left = new ButtonBuilder()
+						.setCustomId('left')
+						.setLabel(leftArrowText)
+						.setStyle(ButtonStyle.Primary);
 
-							const row = new ActionRowBuilder()
-								.addComponents(left, right);
+					const row = new ActionRowBuilder()
+						.addComponents(left, right);
 
-							const response = await interaction.channel.send({
-								embeds: [animeCharacterEmbed],
-								components: [row],
-							});
+					const response = await interaction.channel.send({
+						embeds: [animeCharacterEmbed],
+						components: [row],
+					});
 
-							const collector = response.createMessageComponentCollector({ time: 60000 });
+					const collector = response.createMessageComponentCollector({ time: 60000 });
 
-							collector.on('collect', async buttonInteraction => {
-								try {
-									await buttonInteraction.deferUpdate();
+					collector.on('collect', async buttonInteraction => {
+						try {
+							await buttonInteraction.deferUpdate();
 
-									if (buttonInteraction.customId === 'right') {
-										const updatedEmbed = await animeCharacterSearch.updateCharacterEmbed(true);
-										await buttonInteraction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
-									} else if (buttonInteraction.customId === 'left') {
-										const updatedEmbed = await animeCharacterSearch.updateCharacterEmbed(false);
-										await buttonInteraction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
-									}
-								} catch (error) {
-									console.error(error);
-								}
-							});
-							break;
-					}
+							if (buttonInteraction.customId === 'right') {
+								const updatedEmbed = await animeCharacterSearch.updateCharacterEmbed(true);
+								await buttonInteraction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+							} else if (buttonInteraction.customId === 'left') {
+								const updatedEmbed = await animeCharacterSearch.updateCharacterEmbed(false);
+								await buttonInteraction.editReply({ embeds: [updatedEmbed] }).catch(console.error)
+							}
+						} catch (error) {
+							console.error(error);
+						}
+					});
 				} catch (error) {
 					await interaction.editReply('Something went wrong with getting main character information.');
 					console.error('Error in getAnime: main characters ', error);
 				}
 			});
-
 		} catch (error) {
 			await interaction.editReply('Something went wrong with getting anime information.');
 			console.error('Error in getAnime: anime search', error);
