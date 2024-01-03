07d14aad47a8069e065d7c7d722c8d3601a0f79a
Ares <areszee7@gmail.com>
2024-01-02 18:54:50 -0800

    Update getAnime.js

--- commands/anime/getAnime.js
+++ commands/anime/getAnime.js
@@ -41,7 +41,7 @@ module.exports = {
 			const animeID = await getJikanID('anime', animeName);
 
 			if (!animeID) {
-				await interaction.editReply(`${animeName} not found/listed. DEPLOY`);
+				await interaction.editReply(`${animeName} not found/listed`);
 				return;
 			}
 
